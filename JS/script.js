a8b13526688217641ba09fe959158b7974959b42
Raritetnik <mkodarenko@gmail.com>
2022-08-18 12:44:51 -0400

    HTML, CSS доработан. Начал JS

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+let nom_complet;
+let courriel;
+let numero_telephone;
+
+let date_depart;
+let date_fin;
+let destination;
+
+let quantite_billet = 0;
+
+
+
+document.addEventListener("load", function() {
+    const informationsBase = document.querySelectorAll('.info-personnel');
+    const informationsDate = document.querySelectorAll('.inscription-date');
+    const optionDestination = document.querySelectorAll('.destination');
+});
