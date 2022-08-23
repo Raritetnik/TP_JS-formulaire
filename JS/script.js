5570a92387f2036fa80a524a88b0094988e91386
Raritetnik <mkodarenko@gmail.com>
2022-08-23 12:51:17 -0400

    Ajustement sur le menu et les resolutions d'ecran

--- JS/script.js
+++ JS/script.js
@@ -1,72 +1,46 @@
-class Billet {
-    static i = 0;
-
-    static ajouterBillet(){
-        Billet.i++
-    }
-
-    static numeroBillet() {
-        return "billet-"+ (Billet.i);
-    }
-
-    static supprimerBillet() {
-        Billet.i--;
-    }
-}
-
-let c_formProfil = {
-    nom_complet: "",
-    courriel: "",
-    phone: "",
-};
-let c_formContenu = {
-    dateDepart: "",
-    dateFin: "",
-    destination: "",
-};
-let c_formBillet = [];
-
-
 const e__formProfil = document.querySelector(".form_profil");
 const e__formContenu = document.querySelector(".form_contenu");
 const e__formBillet = document.querySelector(".form_billet");
+const e__resume = document.querySelector(".resume");
 
+// Cacher les formulaire de départ
 window.addEventListener("load", function(){
     e__formContenu.classList.add('invisible');
     e__formBillet.classList.add('invisible');
+    e__resume.classList.add("invisible");
 });
 
-
 /**
- * Ajout des billets
+ * Ajout / Suppression des billets
  */
-const e_billet = e__formBillet.querySelector('.billet');
+const e_billet = e__formBillet.querySelector('.billet').cloneNode(true);
 const e_tableBillets = document.querySelector(".table-billets");
 
  e__formBillet.querySelector('input[type="number"]').addEventListener("change", function() {
-    if(e_tableBillets.childElementCount > -1){
-        do{
-            let e_cloneBillet = e_billet.cloneNode(true);
-            e_cloneBillet.querySelectorAll('input').forEach(element => {
-                element.setAttribute('name', Billet.numeroBillet());
-            });
-            console.log(e_cloneBillet);
-            if(e_tableBillets.childElementCount > this.value) {
-                e_tableBillets.removeChild(e_tableBillets.lastChild);
-                Billet.supprimerBillet();
-            } else if (e_tableBillets.childElementCount < this.value) {
-                e_tableBillets.appendChild(e_cloneBillet);
-                Billet.ajouterBillet();
-            }
+    do{
+        // Création du billet-clone
+        let e_cloneBillet = e_billet.cloneNode(true);
+        e_cloneBillet.querySelector('p').textContent = `Billet #${e_tableBillets.childElementCount+1}`;
+        e_cloneBillet.classList.remove("invisible");
+        e_cloneBillet.querySelectorAll('input').forEach(element => {
+            element.setAttribute('name', `billet-${e_tableBillets.childElementCount+1}`);
+        });
 
-        } while(e_tableBillets.childElementCount != this.value);
-    }
+        // Ajouter ou retirer le billet
+        if(e_tableBillets.childElementCount > this.value) {
+            e_tableBillets.removeChild(e_tableBillets.lastChild);
+        } else if (e_tableBillets.childElementCount <= this.value) {
+            e_tableBillets.appendChild(e_cloneBillet);
+        }
+    } while(e_tableBillets.childElementCount != this.value);
  });
 
-
 /**
- * ========================================================
+ *
  */
+function finalisationDeFormulaire() {
+
+}
 
 /**
  * Formulaire de formulaire -- Actions
@@ -93,8 +67,20 @@ e__formContenu.querySelector('.retour').addEventListener("click", function() {
  */
 e__formBillet.querySelector('.continue').addEventListener("click", function() {
     e__formBillet.classList.add('invisible');
+    e__resume.classList.remove('invisible');
 });
 e__formBillet.querySelector('.retour').addEventListener("click", function() {
     e__formBillet.classList.add('invisible');
     e__formContenu.classList.remove('invisible');
+});
+
+/**
+ * Formulaire de billet -- Actions
+ */
+ e__resume.querySelector('.continue').addEventListener("click", function() {
+    e__resume.classList.add('invisible');
+});
+e__resume.querySelector('.retour').addEventListener("click", function() {
+    e__resume.classList.add('invisible');
+    e__formProfil.classList.remove('invisible');
 });
